638140d5458b89f0cf17a335fa1adaf28906a857
Jani Makela <janimakelaa@gmail.com>
2016-06-23 23:46:13 +0300

    Solve "Pragmatic taxes" exercise
    The idea is to calculate total amounts to orders based on tax rates based
    on areas. If there are no tax rates for the area net_amount is the total_amount.
    
    Really good exercise on pattern matching and the differences between keyword
    lists and maps.

---
+++ programming-elixir/list_recursion/pragmaticTaxes.exs
@@ -0,0 +1,27 @@
+defmodule PragmaticTaxes do
+
+  def pp_tax_rates, do: [ NC: 0.075, TX: 0.08]
+
+  def pp_orders, do: [
+    [ id: 123, ship_to: :NC, net_amount: 100.00 ],
+    [ id: 124, ship_to: :OK, net_amount:  35.50 ],
+    [ id: 125, ship_to: :TX, net_amount:  24.00 ],
+    [ id: 126, ship_to: :TX, net_amount:  44.80 ],
+    [ id: 127, ship_to: :NC, net_amount:  25.00 ],
+    [ id: 128, ship_to: :MA, net_amount:  10.00 ],
+    [ id: 129, ship_to: :CA, net_amount: 102.00 ],
+    [ id: 130, ship_to: :NC, net_amount:  50.00 ] ]
+
+  def add_total_amounts(orders \\ pp_orders, tax_rates \\ pp_tax_rates) do
+    tax_rates = Enum.into tax_rates, %{}
+    for order <- orders, do: add_total_amount(order, tax_rates)
+  end
+  defp add_total_amount(order, tax_rates) do
+    [ id: _, ship_to: to, net_amount: amount] = order
+    tax = tax_rates[to]
+    tax = tax || 0
+    total_amount = amount * (1 + tax)
+    order ++ [total_amount: total_amount]
+  end
+
+end
