502d21752acadeffb37942a11850748168b0333e
Jani Makela <janimakelaa@gmail.com>
2016-06-25 18:46:04 +0300

    Solve "ok! flow control" exercise

--- programming-elixir/control_flow/controlFlow.exs
+++ programming-elixir/control_flow/controlFlow.exs
@@ -9,4 +9,13 @@ defmodule ControlFlow do
     end
   end
 
+  def ok!(tuple) do
+    case tuple do
+      { :ok, data } -> data
+      { :error, reason } when is_bitstring(reason) -> raise reason
+      _ -> raise "ok! is expecting a return of { :ok, data }" <>
+                 "or { :error, string } but instead got something else"
+    end
+  end
+
 end
