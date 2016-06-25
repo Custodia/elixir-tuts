dd89961827b4753fb453f2961db8367cc8e9338c
Jani Makela <janimakelaa@gmail.com>
2016-06-25 18:32:19 +0300

    Solve "FizzBuzz with case matching" exercise

---
+++ programming-elixir/control_flow/controlFlow.exs
@@ -0,0 +1,12 @@
+defmodule ControlFlow do
+
+  def fizzbuzz(n) do
+    case { n, rem(n, 3), rem(n, 5) } do
+      { _, 0, 0 } -> "FizzBuzz"
+      { _, 0, _ } -> "Fizz"
+      { _, _, 0 } -> "Buzz"
+      { n, _, _ } -> n
+    end
+  end
+
+end
