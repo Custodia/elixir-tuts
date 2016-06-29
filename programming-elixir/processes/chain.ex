2b13c62bb31f301a64a8f974839ec554161ead85
Jani Mäkelä <janimakelaa@gmail.com>
2016-06-29 18:36:56 +0000

    Try out process chaining
    One million process spinned up in 2.1 seconds

---
+++ programming-elixir/processes/chain.ex
@@ -0,0 +1,27 @@
+defmodule Chain do
+  def counter(next_pid) do
+    receive do
+      n -> send next_pid, n + 1
+    end
+  end
+
+
+  def create_processes(n) do
+    last = Enum.reduce 1..n, self,
+            fn (_, next_pid) ->
+              spawn(Chain, :counter, [next_pid])
+            end
+
+    send last, 0
+
+    receive do
+      final_answer when is_integer(final_answer) ->
+        "Result is #{inspect(final_answer)}"
+    end
+end
+
+  def run(n) do
+    IO.puts inspect :timer.tc(Chain, :create_processes, [n])
+  end
+
+end
