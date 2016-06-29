1cdae34fdf576e627643a5b11c08afcec85d076e
Jani Mäkelä <jani.makela@pp3.inet.fi>
2016-06-29 10:37:07 +0300

    Make Weatherapp.CLI module skeleton

---
+++ programming-elixir/weatherapp/lib/weatherapp/cli.ex
@@ -0,0 +1,19 @@
+defmodule Weatherapp.CLI do
+
+  @moduledoc """
+  Handle command line parsing and the dispatch to the various
+  functions that end up generating the formatted tables based on the
+  given parameters
+  """
+
+
+  @doc """
+  Main function that command line parameters are passed to.
+
+  Will later parse arguments and pass them to be processed.
+  """
+  def main(argv) do
+    :ok
+  end
+
+end
