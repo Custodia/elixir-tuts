012966fc98cd0a3558f73a6b79e19b5d84b32fd4
Jani Mäkelä <janimakelaa@gmail.com>
2016-07-04 22:11:04 +0000

    Implement external api for StackServer.Server

--- programming-elixir/otp/stackServer/lib/stackServer/stack_server.ex
+++ programming-elixir/otp/stackServer/lib/stackServer/stack_server.ex
@@ -1,6 +1,26 @@
 defmodule StackServer.Server do
   use GenServer
 
+  ####
+  # External API
+
+  def start_link(stack) when is_list(stack) do
+    GenServer.start_link(__MODULE__, stack, name: __MODULE__)
+  end
+
+
+  def pop do
+    GenServer.call __MODULE__, :pop
+  end
+
+
+  def push(val) do
+    GenServer.cast __MODULE__, { :push, val }
+  end
+
+
+  ####
+  # GenServer implementation
 
   def handle_call(:pop, _from, [head | tail]) do
     { :reply, head, tail}
