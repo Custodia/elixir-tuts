79fcdbf2f0847b0135b03dd79ed0525b93f7fc4c
Jani Mäkelä <janimakelaa@gmail.com>
2016-07-05 21:04:16 +0000

    Dont allow non list values into the stash
    Stash is supposed to be robust and always hold valid state.

--- programming-elixir/otp/stackServer/lib/stackServer/stash.ex
+++ programming-elixir/otp/stackServer/lib/stackServer/stash.ex
@@ -4,9 +4,10 @@ defmodule StackServer.Stash do
   ####
   # External API
 
-  def start_link(stack) do
+  def start_link(stack) when is_list(stack) do
     { :ok, _pid } = GenServer.start_link(__MODULE__, stack)
   end
+  def start_link(_), do: { :ok, _pid } = GenServer.start_link(__MODULE__, [])
 
 
   def save_stack(pid, stack) do
@@ -27,8 +28,11 @@ defmodule StackServer.Stash do
   end
 
 
-  def handle_cast({ :save_stack, new_stack }, _stack) do
+  def handle_cast({ :save_stack, new_stack }, _stack) when is_list(new_stack) do
     { :noreply, new_stack }
+  end
+  def handle_cast({ :save_stack, _invalid }, stack) do
+    { :noreply, stack }
   end
 
 end
