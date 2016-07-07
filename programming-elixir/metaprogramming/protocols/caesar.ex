05cfb470bd59c4c72b5fa55a1d8eb13acaeb3b11
Jani Mäkelä <janimakelaa@gmail.com>
2016-07-07 22:06:45 +0000

    Solve "Caesar encryption protocol" exercise
    I think my solution is good. Only thing that's wrong with it is the
    bound_char helper method.
    
    Delegating functions to a shared module is not only cool but really helpful.
    With only a few necessary functions of a protocol implemented you can still
    get access to a lot of functions that compose of these necessary functions.
    
    Moved macros exercises to there own folder.

---
+++ programming-elixir/metaprogramming/protocols/caesar.ex
@@ -0,0 +1,44 @@
+defmodule Caesar.Shared do
+  def rot13(string) do
+    Caesar.encrypt(string, 13)
+  end
+end
+
+defprotocol Caesar do
+  def encrypt(string, shift)
+  defdelegate rot13(string), to: Caesar.Shared
+end
+
+defimpl Caesar, for: BitString do
+  def encrypt(string, shift) do
+    char_list = to_char_list string
+    Caesar.encrypt(char_list, shift)
+  end
+end
+
+defimpl Caesar, for: List do
+  defp bound_char(char) do
+    cond do
+      char < ?a ->
+        ?z + (char - ?a) + 1
+      char > ?z ->
+        ?a + (char - ?z) - 1
+      true      ->
+        char
+    end
+  end
+
+  def encrypt(string, shift) do
+    if Enum.all?(string, &(&1 in ?a..?z)) do
+      Enum.map(string, &(bound_char(&1 + shift)))
+    else
+      throw "Not a lowercase string"
+    end
+  end
+end
+
+IO.puts "#{Caesar.rot13("koira")} should be xbven"
+IO.puts "#{Caesar.rot13('koira')} should be xbven"
+
+IO.puts "#{Caesar.encrypt("xbven", -13)} should be koira"
+IO.puts "#{Caesar.encrypt('xbven', -13)} should be koira"
