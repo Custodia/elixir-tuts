ea3084561b0c77ed87608e981cf84e9af0e72ba5
Jani Mäkelä <janimakelaa@gmail.com>
2016-07-12 20:47:20 +0000

    Add module to test file for easier testing of module flushing

--- coolstuff/memoize/lib/memoize/test.ex
+++ coolstuff/memoize/lib/memoize/test.ex
@@ -1,7 +1,21 @@
 defmodule Testi do
 
-  def sloow do
-    Process.sleep(2000)
+  def sloow(time_out \\ 2000) do
+    Process.sleep(time_out)
+    :ok
+  end
+
+  def error_func do
+    throw "Server should still be on!"
+  end
+
+end
+
+
+defmodule Testi2 do
+
+  def sloow(time_out \\ 2000) do
+    Process.sleep(time_out)
     :ok
   end
 
