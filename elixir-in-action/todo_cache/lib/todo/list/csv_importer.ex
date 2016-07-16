fde8670d4331468999764dcf9508e5d86b9087a7
Jani Makela <janimakelaa@gmail.com>
2016-07-16 13:30:59 +0300

    Implement a server that serves multiple todo lists
    There is one central GenServer that maps pids to
    todo list names. If asked for a todo list that
    doesn't exist it spins up a new empty one.

---
+++ elixir-in-action/todo_cache/lib/todo/list/csv_importer.ex
@@ -0,0 +1,17 @@
+defmodule Todo.List.CsvImporter do
+
+  def import!(file_name) do
+    File.stream!(file_name)
+    |> Enum.map(&parse_line/1)
+    |> Todo.List.new()
+  end
+
+
+  defp parse_line(<< year::bitstring-size(32) >>  <> "/" <>
+                  << month::bitstring-size(16) >> <> "/" <>
+                  << day::bitstring-size(16) >>   <> "," <>
+                  << title::bitstring >>) do
+    %{ date: { year, month, day }, title: String.trim(title)}
+  end
+
+end
